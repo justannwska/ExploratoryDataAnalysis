77c8e2e6499c4817ddc96e6732f8a5ff3540a5a2
justannwska <just.wierzbinska@gmail.com>
2020-02-22 22:14:32 +0100

    Plot1.R and Plot1.png

---
+++ Plot1.R
@@ -0,0 +1,34 @@
+## Plot1.R
+## 
+## 
+## 
+## 2020-02-21
+## 
+## 
+## ###################################
+
+
+## Step1: Reading data
+data <- read.table("household_power_consumption.txt", sep = ";", header = T, stringsAsFactors = F)
+
+
+## Step2: Convert Date/Time classes using str
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+
+## Step3:Extract data for the following days 2007-02-01 and 2007-02-02.
+selected_dataset <- data[which(data$Date %in% as.Date(c("2007-02-01", "2007-02-02"))),] 
+
+
+## Step4: Change column "Global Active Power" to numeric
+selected_dataset$Global_active_power <- as.numeric(selected_dataset$Global_active_power)
+
+
+## Step5: Plot histogram - frequencies of Global Active Power 
+png(filename="plot1.png", width=480, height = 480 )
+# change parameter for axis labels to horizontal to the axis
+par(las=1)
+# plotting
+hist(selected_dataset$Global_active_power, xlab = "Global Active Power (kilowatts)", 
+     ylab = "Frequency", main = "Global Active Power", col = "red")
+dev.off()
