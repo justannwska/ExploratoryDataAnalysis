24d82678775500956485aa614222b623f39a1114
justannwska <just.wierzbinska@gmail.com>
2020-02-23 16:11:06 +0100

    Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -12,7 +12,7 @@
 data <- read.table("household_power_consumption.txt", sep = ";", header = T, stringsAsFactors = F)
 
 
-## Step2: Convert Date/Time classes using str
+## Step2: Convert Date column to date object
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 
 
